623c0165bbd04a376a02ff2167b316b240305fa6
Christian <chrnielsen2003@gmail.com>
2021-10-11 03:16:50 +0200

    start

--- src/components/CardTemp.js
+++ src/components/CardTemp.js
@@ -1,12 +1,12 @@
 import React from 'react';
-require('dotenv')
+/* require('dotenv').config() */
 
 export default function CardTemp() {
 
 
     return (
         <div>
-            
+            <h1>Card</h1>
         </div>
     )
 }
